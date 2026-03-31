291f60a7413cb47f9abbdc24fef3125ffebadfa7
Jeremy Mamani <jeremymamani832@gmail.com>
2026-03-31 09:28:07 -0500

    changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,6 +12,11 @@ public class PlayerController : MonoBehaviour
 
     public float MoveSpeed = 5f;
     public float RotationSpeed = 5f;
+    public float JumpForce = 1.0f;
+    public float gravity = 9.81f;
+    public float verticalVelocity = 0f;
+
+
 
     private void Awake()
     {
@@ -31,17 +37,45 @@ public class PlayerController : MonoBehaviour
 
         inputs.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
         inputs.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        inputs.Player.Jump.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
     }
 
     void Update()
     {
-        transform.Rotate(Vector3.up * moveInput.x * RotationSpeed * Time.deltaTime);
 
-        Vector3 moveDir = transform.forward * moveInput.y * MoveSpeed * Time.deltaTime;
-
-        controller.Move(moveDir);
-
-
+        OnMove();
+        //OnsimpleMove();
 
     }
+    public void OnMove()
+    {
+
+        transform.Rotate(Vector3.up * moveInput.x * RotationSpeed * Time.deltaTime);
+
+        Vector3 moveDir = transform.forward * moveInput.y * MoveSpeed;
+
+        verticalVelocity -= gravity * Time.deltaTime;
+
+        //Vector3 JumpDir = transform.up * moveInput.y * JumpForce;
+
+        moveDir.y = verticalVelocity;
+
+        controller.Move(moveDir*Time.deltaTime);
+   
+    }
+
+    /*public void OnsimpleMove()
+    {
+        transform.Rotate(Vector3.up * moveInput.x * RotationSpeed * Time.deltaTime);
+
+        Vector3 moveDir = transform.forward * moveInput.y * MoveSpeed;
+
+        Vector3 JumpDir = transform.up * moveInput.y * JumpForce; 
+
+        controller.SimpleMove(moveDir);
+    }
+    */
+
+
 }
